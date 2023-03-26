4bdd88398d4a554a53351fdefe8161e7057b3d30
Florian Irsigler <samiclaus@gmail.com>
2023-03-26 04:10:09 +0200

    Initial commit

---
+++ src/main/java/at/irsigler/bandarchive/BandArchiveApplication.java
@@ -0,0 +1,13 @@
+package at.irsigler.bandarchive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BandArchiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BandArchiveApplication.class, args);
+	}
+
+}
